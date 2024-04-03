12b116fab863c92adb67a93d7bad2cc822a4401d
Bianca Jankiewicz <bjanki2@uic.edu>
2024-04-03 03:29:58 -0500

    update

--- NeonBites/Assets/Scripts/Enemy.cs
+++ NeonBites/Assets/Scripts/Enemy.cs
@@ -31,94 +31,84 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-<<<<<<< Updated upstream
-        if (canAttack && Time.time >= nextAttackTime)
-        {
-            float attackDecision = Random.value;
-            float healthFactor = EnemyHealth / 100;
-            float attackThreshold = Mathf.Lerp(0.8f, 0.3f, 1 - healthFactor);
-            if (attackDecision > attackThreshold)
-            {
-                AttackPlayer();
-                nextAttackTime = Time.time + 13f / attackRate;
-=======
-        float distanceToTarget = Vector3.Distance(transform.position, playerHealth.transform.position);
->>>>>>> Stashed changes
+        
+                float distanceToTarget = Vector3.Distance(transform.position, playerHealth.transform.position);
 
-        if (distanceToTarget > 3.0f)
-        {
-            if (canMove)
-            {
-                Vector3 targetDirection = target.position - transform.position;
-                targetDirection.y = 0;
-                Quaternion rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
-                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-                transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-                animator.SetBool("Move", true);
+
+                if (distanceToTarget > 3.0f)
+                {
+                    if (canMove)
+                    {
+                       animator.SetBool("Move", true);
+
+                        Vector3 targetDirection = target.position - transform.position;
+                        targetDirection.y = 0;
+                        Quaternion rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+                        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                    }
+
+                }
+                else
+                {
+
+                    animator.SetBool("Move", false);
+                    if (canAttack && Time.time >= nextAttackTime)
+                    {
+                        float attackDecision = Random.value;
+                        float healthFactor = EnemyHealth / 100;
+                        float attackThreshold = Mathf.Lerp(0.8f, 0.3f, 1 - healthFactor);
+                        if (attackDecision > attackThreshold)
+                        {
+                            AttackPlayer();
+                            nextAttackTime = Time.time + 15f / attackRate;
+
+                        }
+                    }
+                }
             }
 
 
-
-        }
-        else
+        public void AttackPlayer()
         {
-
-            animator.SetBool("Move", false);
-            if (canAttack && Time.time >= nextAttackTime)
+            Debug.Log("Attacking player");
+            if (playerHealth != null)
             {
-                float attackDecision = Random.value;
-                float healthFactor = EnemyHealth / 100;
-                float attackThreshold = Mathf.Lerp(0.8f, 0.3f, 1 - healthFactor);
-                if (attackDecision > attackThreshold)
-                {
-                    AttackPlayer();
-                    nextAttackTime = Time.time + 15f / attackRate;
+                Debug.Log("Player Health is not null");
+                playerHealth.TakeDamage(7f);
+            }
+        }
 
+        public void TakeDamage(float amount)
+        {
+            float attackDistanceToPlayer = Vector3.Distance(transform.position, playerHealth.transform.position);
+            if (attackDistanceToPlayer <= attackDistance)
+            {
+                TMP_EnemyHealth.text = "Enemy Health: " + EnemyHealth.ToString();
+                EnemyHealth -= amount;
+                if (EnemyHealth <= 0f)
+                {
+                    TMP_EnemyHealth.text = "Enemy Health: 0";
+                    Die();
                 }
             }
         }
-    }
 
-    public void AttackPlayer()
-    {
-        Debug.Log("Attacking player");
-        if (playerHealth != null)
+        void Die()
         {
-            Debug.Log("Player Health is not null");
-            playerHealth.TakeDamage(7f);
+            Debug.Log("Enemy died");
+            Destroy(gameObject);
         }
-    }
 
-    public void TakeDamage(float amount)
-    {
-        float attackDistanceToPlayer = Vector3.Distance(transform.position, playerHealth.transform.position);
-        if (attackDistanceToPlayer <= attackDistance)
+
+        public void EnableAttacking()
         {
-            TMP_EnemyHealth.text = "Enemy Health: " + EnemyHealth.ToString();
-            EnemyHealth -= amount;
-            if (EnemyHealth <= 0f)
-            {
-                TMP_EnemyHealth.text = "Enemy Health: 0";
-                Die();
-            }
+            Debug.Log("Enemy is attacking");
+            canAttack = true;
+            canMove = true;
+
         }
-    }
-
-    void Die()
-    {
-        Debug.Log("Enemy died");
-        Destroy(gameObject);
-    }
 
 
-    public void EnableAttacking()
-    {
-        Debug.Log("Enemy is attacking");
-        canAttack = true;
-        canMove = true;
 
     }
-
-
-
-}
